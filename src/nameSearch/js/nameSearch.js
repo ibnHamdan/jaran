038a33bfba197f9ad0912da4127001713d036bc5
Abdurrahman ibnHamdan <ibnHamdan@msn.com>
2017-01-30 15:10:15 +0300

    improved js code by @abdullahsalem

--- src/nameSearch/js/nameSearch.js
+++ src/nameSearch/js/nameSearch.js
@@ -3,37 +3,29 @@ var nameSearch = nameSearch || {};
 (function (nameSearch) {
     nameSearch.init = function(){
          this.getData();
-
-         this.showUsers.getNames();
-
-         this.showUsers.updateText();
-
-         return ;
     };  
 })(nameSearch);
 
 (function (nameSearch) {
     nameSearch.getData = function(){
+        var _this = this;
          $.ajax({
             url: 'https://jsonplaceholder.typicode.com/users',
             method: 'Get'
-        }).then(function (data) {
-            nameSearch.showUsers.getNames(data);
-        });
+        }).done(this.showUsers.getNames.bind(this.showUsers));
     };  
 })(nameSearch);
+
+
 (function (nameSearch) {
     nameSearch.showUsers = {
         usersName : [],
         getNames: function (data) {
             var list = data;
-            for(var prop in list){
-                list.forEach(function (el, index, array) {
-                nameSearch.showUsers.usersName.push(el.name);
-                for (var prop in el) {
-                }
-            });
+            for(var val of list){
+                this.usersName.push(val.name);
             }
+            this.updateText();
         },
         updateText : function(){
             var resultsArea = document.getElementById('resultArea');
