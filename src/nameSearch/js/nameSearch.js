5ca34dd5539248c371f3d8a2dca57ce66684dc0f
Abdurrahman ibnHamdan <ibnHamdan@msn.com>
2017-02-05 16:24:16 +0300

    Added custom name

--- src/nameSearch/js/nameSearch.js
+++ src/nameSearch/js/nameSearch.js
@@ -16,6 +16,21 @@ var nameSearch = nameSearch || {};
     };  
 })(nameSearch);
 
+(function (nameSearch){
+    nameSearch.getCustomName = {
+        _customNameInput : '',
+        _customNameBtn: '',
+        _addCustomeName: function(){
+            var _this = this;
+            $(this._customNameBtn).on('click', function(){
+                var customName = $(_this._customNameInput).val();
+                nameSearch.getNames.usrsName.push(customName)
+            });
+        },
+
+    }
+})(nameSearch);
+
 
 (function (nameSearch) {
     nameSearch.getNames = {
@@ -27,17 +42,47 @@ var nameSearch = nameSearch || {};
                 this.usersName.push(val.name);
             }
             this.updateText();
+            nameSearch.showNames.showNamesEvent();
+            
         },
         updateText : function(){
             var nameInput = document.getElementById(this._namesHiddenInput);
             nameInput.value = nameSearch.getNames.usersName;
-            console.log(nameInput.value)
         }  
     };
 })(nameSearch);
 
-(function (nameSearch){
-    nameSearch.search = function(){
+// (function (nameSearch){
+//     nameSearch.search = function(){
         
+//     }
+// });
+
+(function(nameSearch){
+    nameSearch.showNames = {
+        _showNamesBtn : '#showNamesBtn',
+        _namesTable: '#namesTable',
+        showNamesEvent : function(){
+            $(this._showNamesBtn).on('click', function(){
+                var names = nameSearch.getNames.usersName;
+                // names.forEach(function(val, key){
+                //     var namesTable = document.getElementById('namesTable');
+                //     namesTable.innerHTML = '<tr><td><input type="checkbox" /> <td> <span>'+ val +'</span></td></tr>';
+                //     console.log(val);
+                // });
+                names.forEach(function(val, key){
+                    var namesTable = $(this._namesTable);
+                    namesTable.append('<tr><td><input type="checkbox" /> <td> <span>'+ val +'</span></td></tr>');
+                    console.log(val);
+                });
+                // for(var val of names){
+                //     //var $item = $('<label><input type="checkbox" /> <span>' + data[dataProp].name + '</span></label>');
+                //     //var name = ('<tr><td><input type="checkbox" /> <td> <span>'+ names[val] +'</span></td></tr>');
+                //     var namesTable = document.getElementById('namesTable');
+                //     namesTable.innerHTML = '<tr><td><input type="checkbox" /> <td> <span>'+ val +'</span></td></tr>';
+                //     console.log(val[names]);
+                // }
+            });
+        },
     }
-})
+})(nameSearch);
