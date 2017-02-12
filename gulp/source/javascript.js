bb7311404948849aed51254bc75da7268f887587
Abdurrahman ibnHamdan <ibnHamdan@msn.com>
2017-02-12 19:40:29 +0300

    Initial new project - for test system-js builder

--- gulp/source/javascript.js
+++ gulp/source/javascript.js
@@ -5,9 +5,31 @@ var babelOnError = require('../util/babel-on-error.js');
 var flatten = require('gulp-flatten');
 var maps = require('../maps.js');
 
+var Builder = require('systemjs-builder')
+
+
 gulp.task('source:javascript', () => {
     return gulp.src(maps.JS)
     .pipe(babel()
     .on('error', babelOnError))
     .pipe(gulp.dest('./_build/src/js/'));
 });
+
+
+gulp.task('sys:js', () => {
+      var builder = new Builder()
+
+      builder.config({
+          baseURL: './_build/src/js',
+          packages: {
+                        './src/js': {
+                                defaultExtension: 'js'
+                        } 
+                }            
+          
+      })
+
+  builder
+    .bundle('app.js', './_build/src/js/nameSearch-boundle.js')
+})
+
